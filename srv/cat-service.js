3eea68e7927966f1a8a36958df0e0a52bd81c549
Doa Bonifacio <50482180+doa-b@users.noreply.github.com>
2025-01-10 16:22:22 +0100

    Alternative to using on in init, just implement method with same name as action

--- srv/cat-service.js
+++ srv/cat-service.js
@@ -5,7 +5,7 @@ class CatalogService extends cds.ApplicationService {
         const {Books} = this.entities;
 
         this.after('READ', Books, this.grandDiscount);
-        this.on('submitOrder', this.reduceStock)
+        // this.on('submitOrder', this.reduceStock)
         return super.init();
     }
 
@@ -15,6 +15,29 @@ class CatalogService extends cds.ApplicationService {
                 b.title += '-- 11% Discount!'
             }
         }
+    }
+
+    async submitOrder(book, quantity) {
+        console.log('SUBMITORDER is Called', book, quantity);
+        const {Books} = this.entities;
+
+        if (quantity < 1) {
+            return req.error('The quantity must be greater than 1');
+        }
+
+        const b = await SELECT.one.from(Books).where({ID: book}).columns(b => { b.stock});
+        if (!b) {
+            return req.error('The book does not exist');
+        }
+
+        let { stock } = b;
+        if(quantity > stock) {
+            return req.error(`quantity ${quantity} exceeds ${stock}`);
+        }
+
+        const result = await UPDATE(Books).where({ID: book}).with({ stock: { '-=': quantity}});
+        console.log(result);
+        return { stock: stock - quantity };
     }
 
     async reduceStock(req) {
